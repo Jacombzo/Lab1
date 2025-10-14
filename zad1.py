1f579262f851228959f2d2ebc9a303b24c398b6b
Jacombzo <Jacombzo@users.noreply.github.com>
2025-10-14 23:59:59 +0200

    Zadanie 1

--- zad1.py
+++ zad1.py
@@ -1,9 +1,19 @@
-1 + 2
-1 + 4.5
-3 / 2
-4 / 2
-3 // 2
--3 // 2
-11 % 2
-2 ** 10
-8 ** (1/3)
+a = 1 + 2
+b = 1 + 4.5
+c = 3 / 2
+d = 4 / 2
+e = 3 // 2
+f = -3 // 2
+g = 11 % 2
+h = 2 ** 10
+i = 8 ** (1/3)
+
+print("1. wynik:", a)
+print("2. wynik:", b)
+print("3. wynik:", c)
+print("4. wynik:", d)
+print("5. wynik:", e)
+print("6. wynik:", f)
+print("7. wynik:", g)
+print("8. wynik:", h)
+print("9. wynik:", i)
