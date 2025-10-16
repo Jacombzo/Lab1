df6bea31d30a9cbd4a6761d0082d16a81de1b44c
Jacombzo <Jacombzo@users.noreply.github.com>
2025-10-16 18:02:20 +0200

    Update zadań (dokończone 1, zrobione 2 i 3)

--- zad1.py
+++ zad1.py
@@ -1,12 +1,18 @@
-a = 1 + 2
+a = 1 + 2 #znak + oznacza dodawanie
 b = 1 + 4.5
-c = 3 / 2
+c = 3 / 2 #znak / oznacza dzielenie zmiennoprzecinkowe
 d = 4 / 2
-e = 3 // 2
+e = 3 // 2 #znak // oznacza dzielenie całkowite
 f = -3 // 2
-g = 11 % 2
-h = 2 ** 10
+g = 11 % 2 #znak % oznacza resztę z dzielenia
+h = 2 ** 10 #znak ** oznacza potęgowanie
 i = 8 ** (1/3)
+
+print(int(3.0)) #int konwertuje wartość na liczbę całkowitą
+print(float(3)) #float konwertuje wartość na liczbę zmiennoprzecinkową
+print(float("3")) #str konwertuje napis "3"  na liczbę zmiennoprzecinkową
+print(str(12.4)) #str konwertuje liczbę na ciąg znaków
+print(bool(0)) #bool ma tylko dwie wartości prawda i fałsz 0-zwraca fałsz
 
 print("1. wynik:", a)
 print("2. wynik:", b)
