ec0d81556da27c977867c56aac7ff9c1898b3e74
Jacombzo <Jacombzo@users.noreply.github.com>
2025-10-18 15:47:16 +0200

    zadanie 4 i 5

---
+++ zad4.py
@@ -0,0 +1,4 @@
+cena = 39.99
+rabat = 0.2
+x = cena * (1 - rabat) #cena po rabacie
+print(f"cena po rabacie wynosi {round(x, 2)}") #round zaokrągla daną liczbę do 2 miejsc po przzecinku
---
+++ zad5.py
@@ -0,0 +1,4 @@
+a = int(input("Podaj wartość pierwszego boku prostokątku: "))
+b = int(input("Podaj wartość drugiego boku prostokątu: "))
+print(f"Obwód prostokątku wynosi: {(2 * a) + (2 * b)}")
+print(f"Pole prostokątu wynosi: {a * b}")
