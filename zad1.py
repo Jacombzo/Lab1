de21b20ad69d842dbf74b4065440f44c8e19b612
Jacombzo <Jacombzo@users.noreply.github.com>
2025-10-18 16:30:35 +0200

    update zad1

--- zad1.py
+++ zad1.py
@@ -10,7 +10,7 @@ i = 8 ** (1/3)
 
 print(int(3.0)) #int konwertuje wartość na liczbę całkowitą
 print(float(3)) #float konwertuje wartość na liczbę zmiennoprzecinkową
-print(float("3")) #str konwertuje napis "3"  na liczbę zmiennoprzecinkową
+print(float("3")) #float konwertuje napis "3" na liczbę zmiennoprzecinkową
 print(str(12.4)) #str konwertuje liczbę na ciąg znaków
 print(bool(0)) #bool ma tylko dwie wartości prawda i fałsz 0-zwraca fałsz
 
